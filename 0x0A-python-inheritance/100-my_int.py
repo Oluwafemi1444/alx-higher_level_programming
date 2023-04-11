4654412922680e2820837e73a162f042878b9a0b
Oluwafemi1444 <oberazaq2@gmail.com>
2023-04-11 17:55:40 +0000

    Update all

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,20 @@
+s modules contains a class
+that inherits from int
+"""
+
+
+class MyInt(int):
+    """
+    responds opposite to == and !=
+    """
+    def __eq__(self, other):
+        """
+        returns opposite of equal
+        """
+        return (not super().__eq__(other))
+
+    def __ne__(self, other):
+        """
+        returns opposite of not equal
+        """
+        return (not super().__ne__(other))
