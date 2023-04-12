1a647747bc838c5ef361db9c8e3e6fc7dd97c054
Oluwafemi1444 <oberazaq2@gmail.com>
2023-04-12 18:53:55 +0000

    Commit it

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,20 +1,16 @@
-s modules contains a class
-that inherits from int
+#!/usr/bin/python3
+"""
+class module
 """
 
 
 class MyInt(int):
-    """
-    responds opposite to == and !=
-    """
-    def __eq__(self, other):
-        """
-        returns opposite of equal
-        """
-        return (not super().__eq__(other))
+    """class with int object"""
+
+    def __ee__(self, other):
+        """equal equal method"""
+        return super().__ee__(other)
 
     def __ne__(self, other):
-        """
-        returns opposite of not equal
-        """
-        return (not super().__ne__(other))
+        """not equal method"""
+        return super().__ne__(other)
